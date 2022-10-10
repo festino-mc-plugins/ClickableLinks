f5ac5ea9ce272ce8c14fc609bd3256f82d8c3836
Festino <offestashka@mail.ru>
2022-10-10 17:46:36 +0300

    Fix broken links

--- src/com/festp/handlers/ChatHandler.java
+++ src/com/festp/handlers/ChatHandler.java
@@ -3,6 +3,7 @@ package com.festp.handlers;
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
 
 import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
@@ -64,7 +65,6 @@ public class ChatHandler implements Listener
 			
 			String linkStr = link.getString();
 			String linkClick = applyBrowserEncoding(linkStr);
-			System.out.println(linkClick);
 			if (!link.hasProtocol)
 				linkClick = "https://" + linkClick;
 			command += ",{\"text\":\"" + linkStr + "\",\"underlined\":true,"
@@ -92,9 +92,29 @@ public class ChatHandler implements Listener
 	private String applyBrowserEncoding(String str)
 	{
 		try {
-			String r = URLEncoder.encode(str, StandardCharsets.UTF_8.toString());
-			// TODO optimize partial decode
-			return r.replace("%2F", "/").replace("%3A", ":").replace("%3F", "?").replace("%3D", "=").replace("%26", "&").replace("%2B", "+").replace("%25", "%");
+			HashMap<Character, String> encoding = new HashMap<>();
+			int length = str.length();
+			for (int i = 0; i < length; i++)
+			{
+				char c = str.charAt(i);
+				if (encoding.containsKey(c))
+					continue;
+				// not very optimized?
+				String encoded = URLEncoder.encode("" + c, StandardCharsets.UTF_8.toString());
+				encoding.put(c, encoded);
+			}
+
+			StringBuilder res = new StringBuilder();
+			for (int i = 0; i < length; i++)
+			{
+				char c = str.charAt(i);
+				if (c <= 255)
+					res.append(c);
+				else
+					res.append(encoding.get(c));
+			}
+			
+			return res.toString();
 		} catch (UnsupportedEncodingException e) {
 			e.printStackTrace();
 			return str;
