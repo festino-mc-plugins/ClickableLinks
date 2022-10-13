cf97bbe8adc8960511d2fada21ff868a5661ef1c
Festino <offestashka@mail.ru>
2022-10-13 18:02:57 +0300

    Chatter class, /me and /say support

--- src/com/festp/Main.java
+++ src/com/festp/Main.java
@@ -4,6 +4,7 @@ import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import com.festp.handlers.ChatHandler;
+import com.festp.handlers.SmallCommandsHandler;
 import com.festp.handlers.WhisperHandler;
 
 public class Main extends JavaPlugin
@@ -13,10 +14,15 @@ public class Main extends JavaPlugin
 		Logger.setLogger(getLogger());
     	PluginManager pm = getServer().getPluginManager();
     	
-    	ChatHandler chatHandler = new ChatHandler(this);
+    	Chatter chatter = new Chatter(this);
+    	
+    	ChatHandler chatHandler = new ChatHandler(chatter);
     	pm.registerEvents(chatHandler, this);
     	
-    	WhisperHandler whisperHandler = new WhisperHandler(this, true);
+    	WhisperHandler whisperHandler = new WhisperHandler(chatter, true);
     	pm.registerEvents(whisperHandler, this);
+    	
+    	SmallCommandsHandler smallHandler = new SmallCommandsHandler(chatter);
+    	pm.registerEvents(smallHandler, this);
 	}
 }
---
+++ src/com/festp/handlers/SmallCommandsHandler.java
@@ -0,0 +1,79 @@
+package com.festp.handlers;
+
+import org.bukkit.command.CommandSender;
+import org.bukkit.event.Cancellable;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerCommandPreprocessEvent;
+import org.bukkit.event.server.ServerCommandEvent;
+
+import com.festp.Chatter;
+import com.festp.utils.Link;
+import com.festp.utils.LinkUtils;
+
+public class SmallCommandsHandler implements Listener
+{
+	private static final String ME_COMMAND = "/me";
+	private static final String SAY_COMMAND = "/say";
+	
+	private Chatter chatter;
+	
+	public SmallCommandsHandler(Chatter chatter)
+	{
+		this.chatter = chatter;
+	}
+	
+	@EventHandler
+	public void onServerCommand(ServerCommandEvent event)
+	{
+		onCommand(event, event.getSender(), "/" + event.getCommand());
+	}
+	@EventHandler
+	public void onPlayerCommand(PlayerCommandPreprocessEvent event)
+	{
+		onCommand(event, event.getPlayer(), event.getMessage());
+	}
+
+	private void onCommand(Cancellable event, CommandSender sender, String command)
+	{
+		if (isMeCommand(command))
+			onMeCommand(event, sender, command);
+		else if (isSayCommand(command))
+			onSayCommand(event, sender, command);
+	}
+
+	private void onMeCommand(Cancellable event, CommandSender sender, String command)
+	{
+		String message = command.substring(ME_COMMAND.length()).trim();
+		if (message == "")
+			return;
+		
+		Link link = LinkUtils.selectLink(message, 0);
+		if (link == null)
+			return;
+		
+		chatter.sendFormatted(null, sender, message, "* %1$s %2$s", link);
+		event.setCancelled(true);
+	}
+
+	private void onSayCommand(Cancellable event, CommandSender sender, String command)
+	{
+		String message = command.substring(SAY_COMMAND.length()).trim();
+		if (message == "")
+			return;
+		
+		Link link = LinkUtils.selectLink(message, 0);
+		if (link == null)
+			return;
+
+		chatter.sendFormatted(null, sender, message, "[%1$s] %2$s", link);
+		event.setCancelled(true);
+	}
+
+	private boolean isMeCommand(String command) {
+		return command.startsWith(ME_COMMAND);
+	}
+	private boolean isSayCommand(String command) {
+		return command.startsWith(SAY_COMMAND);
+	}
+}
